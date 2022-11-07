68dac3bc0879da5f450c3a254006ad433d51e6a6
Roger Perez <roger.perezcol@gmail.com>
2022-11-07 13:46:43 -0300

    Project Launch, libraries linked

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import NavBar from "./components/NavBar/NavBar.jsx"
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+           <h1>Pepe</h1>
+    </div>
+  );
+}
+
+export default App;
